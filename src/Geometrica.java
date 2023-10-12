dabd2fa4b6ce58261dde9a86ad43f3c144a818d2
daysources <142356916+daysources@users.noreply.github.com>
2023-10-12 02:01:11 -0300

    Refinamento do return

--- src/Geometrica.java
+++ src/Geometrica.java
@@ -3,7 +3,4 @@ public class Geometrica implements ICalcMedia {
     @Override
     public double CalculaMedia(double nota1, double nota2){return Math.sqrt(nota1*nota2);}
     @Override
-    public String Resultado(double resulmedia)
-    {if(resulmedia>=5){return "Aprovado";}
-    else{return "Reprovado";}
-    }}
+    public String Resultado(double resulmedia){return resulmedia>=5 ? "Aprovado" : "Reprovado";}}
